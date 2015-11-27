a1e8b02b1d691a187baecc4941b91ca9b5fc036f
Deepak Kumar Gupta <deepakkumargupta@Deepaks-MacBook-Pro-2.local>
2015-11-27 17:35:12 +0530

    add dub a dialogue page for script

---
+++ com/frankly/page/DubAdialogue.java
@@ -0,0 +1,7 @@
+package com.frankly.page;
+
+import com.frankly.base.Keywords;
+
+public class DubAdialogue extends Keywords{
+
+}
---
+++ com/frankly/test/VerifyDubAdialogue.java
@@ -0,0 +1,9 @@
+package com.frankly.test;
+
+import com.frankly.base.Keywords;
+import com.frankly.page.DubAdialogue;
+
+public class VerifyDubAdialogue extends Keywords {
+	DubAdialogue dubadialogue = new DubAdialogue();
+
+}
